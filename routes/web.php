f1fa1bf33ca61f2d99a072cc1903a43d8101d1af
zayennn <zaayeennn@gmail.com>
2026-05-01 15:27:30 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,14 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get("/what-i-use", function() {
+Route::get('/what-i-use', function() {
     return view('what-i-use', [
         'starsCount' => 100
     ]);
 })->name('what-i-use');
+
+Route::get('/what-ive-built', function() {
+    return view('what-ive-built', [
+        'starsCount' => 100
+    ]);
+})->name('what-ive-built');
