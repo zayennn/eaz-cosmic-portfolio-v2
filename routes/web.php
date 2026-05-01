4b7dcae5a037b81995b8eb81cad642529baa8540
zayennn <zaayeennn@gmail.com>
2026-05-01 15:17:00 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'starsCount' => 600
+        'starsCount' => 300
     ]);
 })->name('home');
 
