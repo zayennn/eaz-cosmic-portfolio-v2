53184c2acbb78644e72dddb8583e6a4fa78c914e
zayennn <zaayeennn@gmail.com>
2026-05-02 15:55:54 +0700

    update

--- resources/views/what-im-into.blade.php
+++ resources/views/what-im-into.blade.php
@@ -4,10 +4,8 @@
 
 @section('content')
     <section class="into-page" id="intoPage">
-        <!-- Star Map Canvas (transparent - star-field is background) -->
         <canvas id="starMapCanvas"></canvas>
 
-        <!-- Header Overlay -->
         <div class="into-header-overlay">
             <div class="into-badge">
                 <i class="fas fa-star"></i> Explore My Universe
@@ -20,7 +18,6 @@
             </p>
         </div>
 
-        <!-- Category Legend -->
         <div class="category-legend" id="categoryLegend">
             <button class="legend-item active" data-category="all">
                 <span class="legend-dot all"></span> All
@@ -33,7 +30,6 @@
             </button>
         </div>
 
-        <!-- Zoom Controls -->
         <div class="map-controls">
             <button class="map-btn" id="zoomIn" title="Zoom In">
                 <i class="fas fa-plus"></i>
@@ -46,13 +42,11 @@
             </button>
         </div>
 
-        <!-- Minimap -->
         <div class="minimap" id="minimap">
             <canvas id="minimapCanvas"></canvas>
             <div class="minimap-viewport" id="minimapViewport"></div>
         </div>
 
-        <!-- Star Detail Popup -->
         <div class="star-popup" id="starPopup">
             <div class="popup-content" id="popupContent"></div>
         </div>
