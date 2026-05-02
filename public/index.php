602642fae27f26287e8220aaebb032b440c3dfd4
zayennn <zaayeennn@gmail.com>
2026-05-02 21:26:15 +0700

    update

--- public/index.php
+++ public/index.php
@@ -31,7 +31,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+// require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../laravel/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +45,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../laravel/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
